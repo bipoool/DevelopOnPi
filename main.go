4da1e7d1bed870b52b26ad90787f6cfa1eacc3a7
bipoool <itsmevipulgupta.2011@gmail.com>
2025-02-15 15:29:16 +0000

    Added main file

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os/signal"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,11 +12,22 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background())
 	defer stop()
 
+	backendServerStruct := Back{
+		activeContainers: 0,
+		cpuCores:         4,
+		coresOccupied:    0,
+		memory:           16,
+		memoryOccupied:   0,
+		alive:            true,
+		mux:              sync.RWMutex{},
+	}
 	router := gin.Default()
 
 	router.GET("/health", func(ctx *gin.Context) {
 		ctx.Status(200)
 	})
 
-	router.GET("/status")
+	router.GET("/available-cpu", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"availableCpu": 4})
+	})
 }
